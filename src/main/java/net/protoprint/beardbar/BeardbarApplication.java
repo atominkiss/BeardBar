2343e4e6acb52f5eafb60c6b8225443f25656788
atomink <atomink@gmail.com>
2020-04-16 14:06:08 +0300

    Initial commit

---
+++ src/main/java/net/protoprint/beardbar/BeardbarApplication.java
@@ -0,0 +1,13 @@
+package net.protoprint.beardbar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeardbarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeardbarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/protoprint/beardbar/BeardbarApplicationTests.java
@@ -0,0 +1,13 @@
+package net.protoprint.beardbar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeardbarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
